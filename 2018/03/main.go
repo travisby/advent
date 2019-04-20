2490cec2b7f6aa9b93c61ca0156a2519d6aa5761
Travis Beatty <travisby@gmail.com>
2019-04-20 15:11:14 -0400

    [2018][03] p2

--- 2018/03/main.go
+++ 2018/03/main.go
@@ -5,17 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
-// a grid is a x*y matrix with each cell containing the slice of claims that are using it
-type grid [][][]*claim
+// a grid is a x*y matrix with each cell containing the slice of claimIDs that are using it
+type grid [][][]int
 
 func newGrid(x, y int) *grid {
-	g := make([][][]*claim, x)
+	g := make([][][]int, x)
 	for i := range g {
-		g[i] = make([][]*claim, y)
+		g[i] = make([][]int, y)
 		for j := range g[i] {
-			g[i][j] = []*claim{}
+			g[i][j] = []int{}
 		}
 	}
 
@@ -33,22 +34,41 @@ func (g grid) apply(c *claim) error {
 
 	for i := c.point.x; i < (c.point.x + c.width); i++ {
 		for j := c.point.y; j < (c.point.y + c.height); j++ {
-			g[i][j] = append(g[i][j], c)
+			g[i][j] = append(g[i][j], c.id)
 		}
 	}
 	return nil
 }
 
 func (g grid) numContested() int {
-	n := 0
+	return len(g.contested())
+}
+
+func (g grid) contested() [][]int {
+	contested := [][]int{}
 	for i := range g {
 		for j := range g[i] {
 			if len(g[i][j]) > 1 {
-				n++
+				contested = append(contested, g[i][j])
 			}
 		}
 	}
-	return n
+	return contested
+}
+
+func (g grid) contestingIDs() []int {
+	idsDeduplicated := map[int]byte{}
+	for _, cs := range g.contested() {
+		for _, c := range cs {
+			idsDeduplicated[c] = 0x00
+		}
+	}
+
+	ids := make([]int, 0, len(idsDeduplicated))
+	for k := range idsDeduplicated {
+		ids = append(ids, k)
+	}
+	return ids
 }
 
 type claim struct {
@@ -110,5 +130,27 @@ func main() {
 		}
 	}
 	fmt.Printf("p1: %d\n", g.numContested())
+
+	contested := g.contestingIDs()
+	// make it easy to search!
+	sort.Ints(contested)
+	var id *int
+	for _, c := range claims {
+		// sort.SearchInts is weird, it will return the place the ID _should_ go if it were in the list
+		// so check if the item there is actually our ID (or if its' the end of the list, don't try to panic)
+		// if it's not in there already, we found our int
+		potentialPlace := sort.SearchInts(contested, c.id)
+		if len(contested) < potentialPlace {
+			id = &c.id
+			break
+		} else if contested[potentialPlace] != c.id {
+			id = &c.id
+			break
+		}
+	}
+	if id == nil {
+		log.Fatal("Did not find a claim without contention")
+	}
+	fmt.Printf("p2: %d\n", *id)
 
 }
