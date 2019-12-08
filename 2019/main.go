f60e3a6376a8f0a59f6ca3f68f35966fa7f11166
Travis Beatty <travisby@gmail.com>
2019-12-08 07:27:50 -0500

    Answer for 2019.01

---
+++ 2019/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+)
+
+type Module int // the mass of the Module
+
+func MassFromString(s string) (*Module, error) {
+	i, err := strconv.Atoi(s)
+
+	m := Module(i)
+
+	return &m, err
+}
+
+func (m Module) Fuel() int {
+	return int(m)/3 - 2
+}
+
+func main() {
+	var f *os.File
+	if len(os.Args) == 2 {
+		var err error
+		f, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		f = os.Stdin
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	totalFuel := 0
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		module, err := MassFromString(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		totalFuel += module.Fuel()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Fuel required: %d", totalFuel)
+}
