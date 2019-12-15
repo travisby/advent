59c45e4c09ec1503122a71606a4a3edafbc6478a
Travis Beatty <travisby@gmail.com>
2019-12-15 00:05:08 -0500

    brute-forced 2019.02p2

--- 2019/02/main.go
+++ 2019/02/main.go
@@ -13,14 +13,16 @@ import (
 
 func main() {
 	var f *os.File
-	if len(os.Args) == 2 {
+	if len(os.Args) == 3 {
 		var err error
-		f, err = os.Open(os.Args[1])
+		f, err = os.Open(os.Args[2])
 		if err != nil {
 			log.Fatal(err)
 		}
-	} else {
+	} else if len(os.Args) == 2 {
 		f = os.Stdin
+	} else {
+		log.Printf("Missing argument!  Expected app <reverseInputToSearchFor> filename")
 	}
 
 	defer func() {
@@ -61,6 +63,33 @@ func main() {
 
 	log.Printf("Output: %d", virtualMachine.Output())
 	// END PART 1
+
+	// PART 2
+	reverseInputToSearchFor, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// brute force
+	for noun := 0; noun < 100; noun++ {
+		for verb := 0; verb < 100; verb++ {
+			if err := virtualMachine.Reset(); err != nil {
+				log.Fatal(err)
+			} else if err := virtualMachine.SetNoun(noun); err != nil {
+				log.Fatal(err)
+			} else if err := virtualMachine.SetVerb(verb); err != nil {
+				log.Fatal(err)
+			} else if err := virtualMachine.Run(); err != nil {
+				log.Fatal(err)
+			}
+
+			if virtualMachine.Output() == reverseInputToSearchFor {
+				log.Printf("For output=%d, 100*noun+verb=%d", reverseInputToSearchFor, 100*noun+verb)
+				return
+			}
+		}
+	}
+	log.Fatalf("Exhaustive search yielded no result for output=%d", reverseInputToSearchFor)
 }
 
 // shamelessly stolen from ScanWords, but with "," instead of " " as the delimiter
