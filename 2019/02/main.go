0276e5817e62faad7c8a2fc3ae52ceaa782bcba9
Travis Beatty <travisby@gmail.com>
2019-12-14 23:10:39 -0500

    [2019][02] linted

--- 2019/02/main.go
+++ 2019/02/main.go
@@ -11,33 +11,6 @@ import (
 	"gitlab.com/travisby/advent/2019/02/vm"
 )
 
-// shamelessly stolen from ScanWords, but with "," instead of " " as the delimiter
-func ScanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) {
-	// Skip leading spaces.
-	start := 0
-	for width := 0; start < len(data); start += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[start:])
-		if r != ',' {
-			break
-		}
-	}
-	// Scan until space, marking end of word.
-	for width, i := 0, start; i < len(data); i += width {
-		var r rune
-		r, width = utf8.DecodeRune(data[i:])
-		if r == ',' {
-			return i + width, data[start:i], nil
-		}
-	}
-	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
-	if atEOF && len(data) > start {
-		return len(data), data[start:], nil
-	}
-	// Request more data.
-	return start, nil, nil
-}
-
 func main() {
 	var f *os.File
 	if len(os.Args) == 2 {
@@ -59,7 +32,7 @@ func main() {
 	memory := []int{}
 
 	scanner := bufio.NewScanner(f)
-	scanner.Split(ScanCommas)
+	scanner.Split(scanCommas)
 
 	for scanner.Scan() {
 		maybeInt := strings.Trim(scanner.Text(), "\n")
@@ -91,4 +64,31 @@ func main() {
 
 	mem := virtualMachine.Memory[0]
 	log.Printf("Value left at position 0: %d", mem)
+}
+
+// shamelessly stolen from ScanWords, but with "," instead of " " as the delimiter
+func scanCommas(data []byte, atEOF bool) (advance int, token []byte, err error) {
+	// Skip leading spaces.
+	start := 0
+	for width := 0; start < len(data); start += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[start:])
+		if r != ',' {
+			break
+		}
+	}
+	// Scan until space, marking end of word.
+	for width, i := 0, start; i < len(data); i += width {
+		var r rune
+		r, width = utf8.DecodeRune(data[i:])
+		if r == ',' {
+			return i + width, data[start:i], nil
+		}
+	}
+	// If we're at EOF, we have a final, non-empty, non-terminated word. Return it.
+	if atEOF && len(data) > start {
+		return len(data), data[start:], nil
+	}
+	// Request more data.
+	return start, nil, nil
 }
