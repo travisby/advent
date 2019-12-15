e9c7b77182a432b73fafaa96441bfe4c7e249ff1
Travis Beatty <travisby@gmail.com>
2019-12-14 22:49:42 -0500

    Utilized new program class + tests in vm

--- 2019/02/vm/vm.go
+++ 2019/02/vm/vm.go
@@ -1,28 +1,47 @@
 package vm
 
 import (
-	"fmt"
+	"errors"
 
-	"gitlab.com/travisby/advent/2019/02/program"
+	"gitlab.com/travisby/advent/2019/02/vm/program"
 )
+
+var ErrOverflow = errors.New("The memory has overflowed")
 
 // VM is our VirtualMachine that runs IntCode
 type VM struct {
-	Memory  []int // the state of memory in the VM
-	Program program.Program
+	memory []int // the state of memory in the VM
 }
 
 // New creates a new Virtual Machine
-func New() *VM {
-	return &VM{}
+func New(memorySize int) *VM {
+	// align to 4 ints
+	if memorySize%4 != 0 {
+		memorySize = (memorySize/4 + 1) * 4
+	}
+
+	return &VM{memory: make([]int, memorySize)}
 }
 
 // Load an intcode program into memory
-func (v *VM) Load([]int) error {
-	return fmt.Errorf("Not implemented")
+func (v *VM) Load(offset int, ints []int) error {
+	if len(ints)+offset > len(v.memory) {
+		return ErrOverflow
+	}
+
+	for i := range ints {
+		v.memory[offset+i] = ints[i]
+	}
+
+	return nil
 }
 
 // Run the loaded program
 func (v *VM) Run() error {
-	return fmt.Errorf("Not implemented")
+	p := program.NewScanner(v.memory)
+	for p.Scan() {
+		p.Intcode().Apply(v.memory)
+
+	}
+	return p.Err()
 }
