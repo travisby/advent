fb8f8c64ff93921d9b78002bc834d8c6dfd8ee25
Travis Beatty <travisby@gmail.com>
2020-12-02 07:40:08 -0500

    Added 2020.02.a question

---
+++ 2020/02/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	var f *os.File
+	if len(os.Args) == 2 {
+		var err error
+		f, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		f = os.Stdin
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		_ = scanner.Text()
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
