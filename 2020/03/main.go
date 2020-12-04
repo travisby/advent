091fc428465349441071cf5ccd7c29b2acad5fdd
Travis Beatty <travisby@gmail.com>
2020-12-03 21:10:43 -0500

    Solved 2020.03.a

---
+++ 2020/03/main.go
@@ -0,0 +1,111 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+)
+
+var ErrUnknownTreeSyntax = errors.New("Unknown tree syntax")
+var ErrTraversal = errors.New("Traversing into the unknown")
+
+type treeLayer struct {
+	trees []bool
+}
+
+func newTreeLayer(s string) (*treeLayer, error) {
+	t := treeLayer{make([]bool, len(s))}
+
+	for i, c := range s {
+		if c == '.' {
+			t.trees[i] = false
+		} else if c == '#' {
+			t.trees[i] = true
+		} else {
+			return nil, fmt.Errorf("%w at pos %d, %q", ErrUnknownTreeSyntax, i, c)
+		}
+	}
+
+	return &t, nil
+}
+
+type treeMap struct {
+	layers []treeLayer
+	curPos struct {
+		x int
+		y int
+	}
+	treesEncountered int
+}
+
+func (t *treeMap) addLayer(ts treeLayer) {
+	t.layers = append(t.layers, ts)
+}
+
+// traverse returns false if there's no where left to traverse
+// (AKA we've reached the bottom)
+// traversel L-R is infinite
+// traversal will return false and prevent overtravel
+func (t *treeMap) traverse(x, y int) (more bool) {
+	if t.curPos.y+y >= len(t.layers) {
+		return false
+	}
+
+	t.curPos.x += x
+	t.curPos.y += y
+
+	if t.layers[t.curPos.y].trees[t.curPos.x%len(t.layers[t.curPos.y].trees)] {
+		t.treesEncountered++
+	}
+
+	if t.curPos.y == len(t.layers)-1 {
+		return false
+	}
+
+	return true
+}
+
+func newTreeMap() *treeMap {
+	return &treeMap{}
+}
+
+func main() {
+	var f *os.File
+	if len(os.Args) == 2 {
+		var err error
+		f, err = os.Open(os.Args[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		f = os.Stdin
+	}
+
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	treeMap := newTreeMap()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		layer, err := newTreeLayer(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		treeMap.addLayer(*layer)
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for more := true; more; more = treeMap.traverse(3, 1) {
+	}
+
+	fmt.Printf("%d\n", treeMap.treesEncountered)
+}
---
+++ 2020/03/main_test.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"testing"
+)
+
+func assertLayersEqual(actual, expected *treeLayer) error {
+	if actual == nil && expected != nil {
+		return fmt.Errorf("Unexpected nil treeLayer")
+	} else if actual != nil && expected == nil {
+		return fmt.Errorf("expected nil treeLayer got %+v", *actual)
+	} else if actual != nil && expected != nil {
+		if len(actual.trees) != len(expected.trees) {
+			return fmt.Errorf("Expected equal layers, got actual=%+v expected=%+v", actual.trees, expected.trees)
+		}
+		for i := range actual.trees {
+			if actual.trees[i] != expected.trees[i] {
+				return fmt.Errorf("Expected equal layers, got actual=%+v expected=%+v", actual.trees, expected.trees)
+			}
+		}
+	}
+
+	return nil
+}
+
+func TestNewTreeLayer(t *testing.T) {
+	type test struct {
+		name  string
+		input string
+		err   error
+		layer *treeLayer
+	}
+	tests := []test{
+		{
+			"Base Case",
+			"",
+			nil,
+			&treeLayer{[]bool{}},
+		},
+		{
+			"Simple Case 1",
+			".",
+			nil,
+			&treeLayer{[]bool{false}},
+		},
+		{
+			"Simple Case 2",
+			"#",
+			nil,
+			&treeLayer{[]bool{true}},
+		},
+		{
+			"Err Case",
+			"@",
+			ErrUnknownTreeSyntax,
+			nil,
+		},
+	}
+
+	// here's the sample input given to us from AoC
+	for i, sample := range []struct {
+		s  string
+		bs []bool
+	}{
+		{"..##.......", []bool{false, false, true, true, false, false, false, false, false, false, false}},
+		{"#...#...#..", []bool{true, false, false, false, true, false, false, false, true, false, false}},
+		{".#....#..#.", []bool{false, true, false, false, false, false, true, false, false, true, false}},
+		{"..#.#...#.#", []bool{false, false, true, false, true, false, false, false, true, false, true}},
+		{".#...##..#.", []bool{false, true, false, false, false, true, true, false, false, true, false}},
+		{"..#.##.....", []bool{false, false, true, false, true, true, false, false, false, false, false}},
+		{".#.#.#....#", []bool{false, true, false, true, false, true, false, false, false, false, true}},
+		{".#........#", []bool{false, true, false, false, false, false, false, false, false, false, true}},
+		{"#.##...#...", []bool{true, false, true, true, false, false, false, true, false, false, false}},
+		{"#...##....#", []bool{true, false, false, false, true, true, false, false, false, false, true}},
+		{".#..#...#.#", []bool{false, true, false, false, true, false, false, false, true, false, true}},
+	} {
+		tests = append(
+			tests,
+			test{
+				fmt.Sprintf("AOC Sample %d", i),
+				sample.s,
+				nil,
+				&treeLayer{sample.bs},
+			},
+		)
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			layer, err := newTreeLayer(test.input)
+			if !errors.Is(err, test.err) {
+				t.Fatalf("Expected %v but got %v", test.err, err)
+			}
+			if err := assertLayersEqual(layer, test.layer); err != nil {
+				t.Fatal(err)
+			}
+		})
+	}
+}
+
+func TestNewMapIsEmpty(t *testing.T) {
+	m := newTreeMap()
+	if m == nil {
+		t.Fatal("Did not expect tree map to be nil")
+	} else if len(m.layers) != 0 {
+		t.Fatalf("Expected there to be zero layers. Got %d layers", len(m.layers))
+	}
+}
+
+func TestAddLayer(t *testing.T) {
+	m := treeMap{}
+	treeLayer := treeLayer{[]bool{true}}
+	m.addLayer(treeLayer)
+
+	if len(m.layers) != 1 {
+		t.Fatalf("Expected there to be one and only one layer now. Got %d layers", len(m.layers))
+	}
+
+	if err := assertLayersEqual(&m.layers[0], &treeLayer); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestTraversal(t *testing.T) {
+	type test struct {
+		name    string
+		treeMap treeMap
+		input   struct {
+			x int
+			y int
+		}
+		more   bool
+		curPos struct {
+			x int
+			y int
+		}
+		trees int
+	}
+	tests := []test{
+		{
+			"Simple Case",
+			treeMap{layers: []treeLayer{treeLayer{[]bool{false}}, treeLayer{[]bool{false}}}},
+			struct {
+				x int
+				y int
+			}{0, 0},
+			true,
+			struct {
+				x int
+				y int
+			}{0, 0},
+			0,
+		},
+		{
+			"Down 1 Right 1 Case No Trees",
+			treeMap{layers: []treeLayer{treeLayer{[]bool{false, false}}, treeLayer{[]bool{false, false}}}},
+			struct {
+				x int
+				y int
+			}{1, 1},
+			false,
+			struct {
+				x int
+				y int
+			}{1, 1},
+			0,
+		},
+		{
+			"Down 1 Right 2 Case Some Trees",
+			treeMap{layers: []treeLayer{treeLayer{[]bool{true, false, true}}, treeLayer{[]bool{false, false, true}}}},
+			struct {
+				x int
+				y int
+			}{2, 1},
+			false,
+			struct {
+				x int
+				y int
+			}{2, 1},
+			1,
+		},
+		{
+			"Moving Right should be a repeating pattern",
+			treeMap{layers: []treeLayer{treeLayer{[]bool{true, false}}}},
+			struct {
+				x int
+				y int
+			}{10, 0},
+			false,
+			struct {
+				x int
+				y int
+			}{10, 0},
+			1,
+		},
+	}
+
+	for _, tst := range tests {
+		t.Run(tst.name, func(t *testing.T) {
+			more := tst.treeMap.traverse(tst.input.x, tst.input.y)
+			if more != tst.more {
+				t.Errorf("Expected more %v got %v", tst.more, more)
+			}
+			if tst.treeMap.curPos != tst.curPos {
+				t.Errorf("Expected new curPos to be: %+v got %+v", tst.treeMap.curPos, tst.curPos)
+			}
+			if tst.trees != tst.treeMap.treesEncountered {
+				t.Errorf("Expected trees %d got %d", tst.trees, tst.treeMap.treesEncountered)
+			}
+		})
+	}
+}
