c9a0ac2f4ecbf50e2a372cf15cb9872c0de80d2c
Travis Beatty <travisby@gmail.com>
2020-12-12 09:00:24 -0500

    [2020][12][p2] Added Solution

--- 2020/12/main.go
+++ 2020/12/main.go
@@ -23,6 +23,9 @@ const (
 type plane struct {
 	d *direction
 	p *image.Point
+	// waypoint is RELATIVE to the ship
+	// not in absolute space
+	w *image.Point
 }
 
 func (p plane) manhattanDistance() int {
@@ -35,12 +38,14 @@ func newPlane() *plane {
 	var temp direction = East
 	p.d = &temp
 	p.p = &image.Point{}
+	p.w = &image.Point{10, 1}
 
 	return &p
 }
 
 type Instruction interface {
 	Apply(*plane)
+	Apply2(*plane)
 }
 
 var ErrInvalidInstruction = errors.New("Invalid instruction")
@@ -77,11 +82,17 @@ type north int
 func (n north) Apply(p *plane) {
 	*p.p = p.p.Add(image.Point{0, int(n)})
 }
+func (n north) Apply2(p *plane) {
+	*p.w = p.w.Add(image.Point{0, int(n)})
+}
 
 type south int
 
 func (s south) Apply(p *plane) {
 	*p.p = p.p.Add(image.Point{0, -int(s)})
+}
+func (s south) Apply2(p *plane) {
+	*p.w = p.w.Add(image.Point{0, -int(s)})
 }
 
 type east int
@@ -89,11 +100,17 @@ type east int
 func (e east) Apply(p *plane) {
 	*p.p = p.p.Add(image.Point{int(e), 0})
 }
+func (e east) Apply2(p *plane) {
+	*p.w = p.w.Add(image.Point{int(e), 0})
+}
 
 type west int
 
 func (w west) Apply(p *plane) {
 	*p.p = p.p.Add(image.Point{-int(w), 0})
+}
+func (w west) Apply2(p *plane) {
+	*p.w = p.w.Add(image.Point{-int(w), 0})
 }
 
 type left int
@@ -113,11 +130,30 @@ func (l left) Apply(p *plane) {
 		log.Fatal("Not one of the four angles we expected")
 	}
 }
+func (l left) Apply2(p *plane) {
+	turnsToMove := int(l) / 90
+
+	// (10,4) -> (-4, 10)
+	for turnsToMove > 0 {
+		turnsToMove--
+		*p.w = image.Point{-p.w.Y, p.w.X}
+
+	}
+
+	// (10,4) -> (4, -10)
+	for turnsToMove < 0 {
+		turnsToMove++
+		*p.w = image.Point{p.w.Y, -p.w.X}
+	}
+}
 
 type right int
 
 func (r right) Apply(p *plane) {
 	left(-r).Apply(p)
+}
+func (r right) Apply2(p *plane) {
+	left(-r).Apply2(p)
 }
 
 type forward int
@@ -133,6 +169,9 @@ func (f forward) Apply(p *plane) {
 	case West:
 		west(f).Apply(p)
 	}
+}
+func (f forward) Apply2(p *plane) {
+	*p.p = p.p.Add(p.w.Mul(int(f)))
 }
 
 func main() {
@@ -154,6 +193,7 @@ func main() {
 	}()
 
 	p := newPlane()
+	var insts []Instruction
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -161,11 +201,21 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		i.Apply(p)
+		insts = append(insts, i)
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%d", p.manhattanDistance())
+	// Part 1
+	for _, i := range insts {
+		i.Apply(p)
+	}
+	log.Printf("Part 1: %d", p.manhattanDistance())
+
+	p2 := newPlane()
+	for _, i := range insts {
+		i.Apply2(p2)
+	}
+	log.Printf("Part 2: %d", p2.manhattanDistance())
 }
