ab8a025ceda57eda837c4d4ae3d8c5aff39918f1
Travis Beatty <travisby@gmail.com>
2021-12-01 19:49:02 -0500

    2021/01/b

--- 2021/main.go
+++ 2021/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"container/ring"
 	"log"
 	"os"
 	"strconv"
@@ -25,8 +26,13 @@ func main() {
 		}
 	}()
 
-	var oldI *int
 	var increase int
+	var slidingIncrease int
+
+	// to have two separate 3-measurement windows
+	// we only need 4 elements
+	// A B C D -> [A B C] [B C D]
+	intRing := ring.New(4)
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
@@ -34,15 +40,45 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		intRing.Value = &i
 
-		if oldI != nil && i > *oldI {
+		// part a maths
+		if intRing.Prev().Value != nil && *intRing.Value.(*int) > *intRing.Prev().Value.(*int) {
 			increase++
 		}
-		oldI = &i
+
+		// I got pretty stuck on doing this code here
+		// I really would have liked Move() and Unlink() to have helped me
+		// get "the next 3" or "the previous 3" and throw them into an summation function
+		// unfortunately it would fail for some reason every time
+		// the best I can think of is it does not like partially empty rings; the comments
+		// for Unlink do say that r must not be nil, and assuming their implementation
+		// at some point it is nil
+
+		// part b maths, super panicy code
+		// only do if the ring buffer is full!
+		var notFull bool
+		intRing.Do(func(i interface{}) {
+			if i == nil {
+				notFull = true
+			}
+		})
+
+		if !notFull {
+			prev := *intRing.Next().Value.(*int) + *intRing.Next().Next().Value.(*int) + *intRing.Next().Next().Next().Value.(*int)
+			next := *intRing.Value.(*int) + *intRing.Prev().Value.(*int) + *intRing.Prev().Prev().Value.(*int)
+
+			if next > prev {
+				slidingIncrease++
+			}
+		}
+
+		intRing = intRing.Next()
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("%d", increase)
+	log.Printf("part 1: %d", increase)
+	log.Printf("part 2: %d", slidingIncrease)
 }
